a79d807c27660f84e10a993df1d68a68d1798617
vatrapusandhya <sandhyavatrapu@Sandhyas-MBP.attlocal.net>
2022-09-03 13:12:28 -0500

    intial commit

---
+++ src/browser/Browser.java
@@ -0,0 +1,24 @@
+package browser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Invoke Browser
+		//chrome - ChromeDriver
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/sandhyavatrapu/Documents/chromedriver");
+		// will get all the chromeDriver methods which are in webDriver interface
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/");
+		
+		
+
+	}
+
+}
